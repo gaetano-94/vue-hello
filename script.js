0df34b0a7af8d92224c5e09edc7428cf086f212f
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-24 15:54:22 +0100

    feat: h1 con vue js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: 'Ciao Vue JS',
+      mieClassi: 'text text-2',
+    };
+  },
+}).mount('#title');
